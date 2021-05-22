339737362db72a5dfe8e59c2fa39fd92f0d8a94b
Esben Viskum <viskum@viskum.dk>
2021-05-22 15:39:00 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Esben Viskum on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
